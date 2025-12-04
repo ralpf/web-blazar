c90c01876e8a8a713258cce9359c8fc75e443290
Vladimir Munteanu <vlmu@trackman.com>
2025-12-05 00:29:54 +0200

    add methods to Assert class

--- src/unitlib/Assert.ts
+++ src/unitlib/Assert.ts
@@ -1,5 +1,17 @@
 export class Assert {
 
+    static True(condition: boolean, msg = 'assert failed' ) {
+        if (!condition) throw new Error(`Not True: ${msg}`);
+    }
+
+    static False(condition: boolean, msg = 'assert failed' ) {
+        if (condition) throw new Error(`Not False: ${msg}`);
+    }
+
+    static Index(array: any[], i: number, msg = 'index out of bounds') {
+        if (i < 0 || i >= array.length) throw new Error(`IOR ${array.length}[${i}]: ${msg}`);
+    }
+
     static IsType(instance: any, className: Function): void {
         if (instance instanceof className) return;
         const got = (instance as any)?.constructor?.name ?? typeof instance;
