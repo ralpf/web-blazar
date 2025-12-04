568d30b439fdb7094a3e82d17c3eae504e8ad3b5
Vladimir Munteanu <vlmu@trackman.com>
2025-12-05 00:29:12 +0200

    add class Pages

---
+++ src/unitlib/containers/Pages.ts
@@ -0,0 +1,43 @@
+import { Unit } from "../Unit";
+import { Assert } from "../Assert";
+
+
+/**
+ * A container for content, to show one children at a time
+ */
+export class Pages extends Unit {
+
+    private all: Unit[] = [];
+    private idx: number;
+
+    get count(): number     { return this.all.length; }
+    get activeUnit(): Unit  { return this.all[this.idx]; }
+
+    get activeIdx(): number { return this.idx; }
+    set activeIdx(i:number) { this.activateIdx(i); }
+
+
+    constructor(root: HTMLElement) {
+        super(root);
+        //let contentRoot = Unit.FindIn(this.root, '[data-tag="container"]');
+
+        for (const x of this.root.children)
+            this.all.push(new Unit(x as HTMLElement));
+
+        this.idx = this.all.findIndex(x => x.isVisible);
+        if (this.idx < 0) this.idx = 0;
+
+        this.all.forEach(x => x.hide());
+        this.activeUnit.show();
+    }
+
+    private activateIdx(i: number) {
+        Assert.Index(this.all, i);
+        this.activeUnit.hide();
+        this.idx = i;
+        this.activeUnit.show();
+    }
+
+}
+
+// TODO: add next() , preve()
