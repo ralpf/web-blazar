baae639ee65e276d38ff2e82db236fd102fde3ca
Vladimir Munteanu <vlmu@trackman.com>
2025-12-23 02:31:07 +0200

    wip

--- src/proj/views/DeckView.ts
+++ src/proj/views/DeckView.ts
@@ -1,19 +1,26 @@
 import { Unit } from "../../unitlib/Unit";
 import { CompositeUnit } from "../../unitlib/containers/CompositeUnit";
+import { Container } from "../../unitlib/containers/Container";
+import { Dropdown } from "../../unitlib/inputs/Dropdown";
+import { Slider } from "../../unitlib/inputs/Slider";
 
 
 export class DeckView extends CompositeUnit {
 
-    constructor(root: HTMLElement, parent: Unit) {
-        super(root, parent);
-    }
+    private luma!: Slider;
+    private modeSelect!: Dropdown;
+    private modeContent!: Container;
 
-    public initializeClassFields(): void {
-        throw new Error("Method not implemented.");
+
+    protected initializeClassFields(): void {
+        this.luma = this.getField('luma');
+        this.modeSelect = this.getField('modeSelect');
+        this.modeContent = this.getField('modeContent');
     }
 
     protected initializeEvents(): void {
-        throw new Error("Method not implemented.");
+        this.luma.callback = (value: number) => this.propagateURL(`my val is ${value}`);
+        this.modeSelect.callback = (idx: number) => this.modeContent.activeIdx = idx;
     }
 
 }
