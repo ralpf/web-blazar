1b451deecc506873ded06306b3edb6bb3bc9ee47
Vladimir Munteanu <vlmu@trackman.com>
2026-01-10 23:39:24 +0200

    udpate RequestDispatcher

--- unitlib/static/RequestDispatcher.ts
+++ unitlib/static/RequestDispatcher.ts
@@ -3,8 +3,11 @@ import { log } from "../core/global";
 
 export class RequestDispatcher {
 
+    private static baseUrl = `http://${window.location.host}`;
+
+
     public static process(url: string) {
-        log(`ba url ${url}`);
+        log(`ba url ${url} -> TO -> ${this.baseUrl}`);
     }
 
 }
