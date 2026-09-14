a02aa896c5eac1c8a7147a4c8b377e43a2162ab7
Vladimir Munteanu <vlmu@trackman.com>
2026-09-14 23:07:13 +0300

    add shoot confety to star click

--- src/views/ViewsManager.ts
+++ src/views/ViewsManager.ts
@@ -1,11 +1,11 @@
 import { CompositeUnit } from "unitlib/containers/CompositeUnit";
 import { Container } from "unitlib/containers/Container";
 import { Assert } from "unitlib/core/Assert";
-import { Unit } from "unitlib/core/Unit";
 import { ButtonsRowSig } from "unitlib/inputs/ButtonsRowSig";
 import { InputUnit } from "unitlib/inputs/InputUnit";
-import { Coroutine } from "unitlib/static/Coroutine";
 import { AnimationFX } from "../anim/AnimationFX";
+
+import confetti from "canvas-confetti";
 
 
 export class ViewsManager extends CompositeUnit {
@@ -57,9 +57,39 @@ private registerBlazarClick() {
         const span = document.getElementById('ID-span-star');
         Assert.Defined(span);
         span.addEventListener('click', () => {
+            this.shootParticles(span);  // Particle FX
             if (this.isStarAnimated) return;
             AnimationFX.startHeader(span,() => this.isStarAnimated = true, () => this.isStarAnimated = false);
         });
+    }
+
+    private shootParticles(element: HTMLElement) {
+        // move this method to some other place, later
+        const rect = element.getBoundingClientRect();
+
+        confetti({
+            particleCount: 30,
+            spread: 360,
+            startVelocity: 15,
+            origin: {
+                x: (rect.left + rect.width / 2) / window.innerWidth,
+                y: (rect.top + rect.height / 2) / window.innerHeight
+            },
+            angle: 0, // Down and right.
+        });
+        
+        setTimeout(() => {
+            confetti({
+                particleCount: 160,
+                spread: 70,
+                startVelocity: 25,
+                origin: {
+                    x: (rect.left + rect.width / 2) / window.innerWidth,
+                    y: (rect.top + rect.height / 2) / window.innerHeight
+                },
+                angle: 240, // Down and right.
+            });
+        }, 150); // Delay in milliseconds.
     }
 
 }
